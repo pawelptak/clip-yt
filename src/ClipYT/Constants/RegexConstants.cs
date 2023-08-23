7ea94687203f1bee9aca08e72634295030bd8cc6
Paweł Ptak <pawel.ptak@intive.com>
2023-08-23 18:36:18 +0200

    add script that detect youtube url

---
+++ src/ClipYT/Constants/RegexConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ClipYT.Constants
+{
+    public static class RegexConstants
+    {
+        public const string YoutubeUrlRegex = @"^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$";
+        public const string TimeformatRegex = @"^(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d$";
+    }
+}
