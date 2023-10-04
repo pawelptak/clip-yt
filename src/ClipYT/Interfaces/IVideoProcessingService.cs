48f65ca08bcb8d56556b10e34d7558bb4f110703
pawelptak <pawelptak>
2023-10-04 21:04:40 +0200

    Use ffmpeg and yt-dlp directly instead of nugets. Rename VideoDownloaderService to VideoProcessingService. Code cleanup.

---
+++ src/ClipYT/Interfaces/IVideoProcessingService.cs
@@ -0,0 +1,9 @@
+﻿using ClipYT.Models;
+
+namespace ClipYT.Interfaces
+{
+    public interface IVideoProcessingService
+    {
+        Task<FileModel> ProcessYoutubeVideoAsync(VideoModel model);
+    }
+}
