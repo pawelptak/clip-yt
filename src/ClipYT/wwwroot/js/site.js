10bbdad55b4750ebe07719da97fc74928d7dbb94
pawelptak <pawelptak>
2024-09-01 13:51:36 +0200

    add processing indicator

--- src/ClipYT/wwwroot/js/site.js
+++ src/ClipYT/wwwroot/js/site.js
@@ -1,4 +1,36 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('#submit-button').click(function (e) {
+        e.preventDefault();
 
-// Write your JavaScript code.
+        var $button = $(this);
+        $button.addClass('rotating');
+
+        $.ajax({
+            url: '/Home/DownloadVideo',
+            type: 'POST',
+            data: $('form').serialize(),
+            xhrFields: {
+                responseType: 'blob'
+            },
+            success: function (data, status, xhr) {
+                var disposition = xhr.getResponseHeader('Content-Disposition');
+                var filename = disposition.match(/filename="(.+)"/)[1];
+
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(data);
+                link.download = filename;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                $button.removeClass('rotating');
+            },
+            error: function (xhr, status, error) {
+                console.error('AJAX Request failed:', status, error);
+                console.error('Response Text:', xhr.responseText);
+
+                $button.removeClass('rotating');
+            }
+        });
+    });
+});
