5867ca6d352d4053f5c8c6994617d978bfee5d5b
pawelptak <97pawelptak@gmail.com>
2024-10-03 20:33:39 +0200

    Move all js scripts to separate files

---
+++ src/ClipYT/wwwroot/js/ajax-download.js
@@ -0,0 +1,54 @@
+﻿$(document).ready(function () {
+    const appData = document.getElementById('app-data');
+    const downloadMethodUrl = appData.getAttribute('data-download-url');
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+
+        if (!$(this).valid()) {
+            return;
+        }
+
+        var $button = $('#submit-button');
+        $button.addClass('rotating');
+
+        $.ajax({
+            url: downloadMethodUrl,
+            type: 'POST',
+            data: $(this).serialize(),
+            xhrFields: {
+                responseType: 'blob'
+            },
+            success: function (data, status, xhr) {
+                var disposition = xhr.getResponseHeader('Content-Disposition');
+                var filename = '';
+
+                var filenameUTF8 = disposition.match(/filename\*\=UTF-8''([^;]+)/);
+                if (filenameUTF8) {
+                    filename = decodeURIComponent(filenameUTF8[1]);
+                } else {
+                    var filenameNormal = disposition.match(/filename="([^"]+)"/);
+                    if (filenameNormal) {
+                        filename = filenameNormal[1];
+                    }
+                }
+
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(data);
+                link.download = filename;
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                $button.removeClass('rotating');
+            },
+            error: function (xhr, status, error) {
+                console.error('AJAX Request failed:', status, error);
+                console.error('Response Text:', xhr.responseText);
+                $('#url-validation-message').text('An error occurred. Please try again.').show();
+
+                $button.removeClass('rotating');
+            }
+        });
+    });
+});
---
+++ src/ClipYT/wwwroot/js/url-input-listener.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    const appData = document.getElementById('app-data');
+    const ytRegex = appData.getAttribute('data-yt-regex');
+    const tiktokRegex = appData.getAttribute('data-tiktok-regex');
+    const clipytLogoUrl = appData.getAttribute('data-clipyt-logo');
+    const cliptokLogoUrl = appData.getAttribute('data-cliptok-logo');
+
+    $("#urlInput").on('input', function () {
+        var inputUrl = $(this).val();
+
+        if (inputUrl.match(ytRegex)) {
+            updateVideoFrame($(this).val());
+            $("#video-details").show();
+            setClipytMode();
+        }
+        if (inputUrl.match(tiktokRegex)) {
+            $("#player-container").hide();
+            $("#video-details").show();
+            setCliptokMode();
+        }
+    });
+
+    function setCliptokMode() {
+        const body = document.body;
+        if (!body.classList.contains('tiktok-mode')) {
+            body.classList.remove('yt-mode');
+            body.classList.add('tiktok-mode');
+            $("#logo-img").attr('src', cliptokLogoUrl);
+            $("#get-current-end-btn").hide();
+            $("#get-current-start-btn").hide();
+        }
+    }
+
+    function setClipytMode() {
+        const body = document.body;
+        if (!body.classList.contains('yt-mode')) {
+            body.classList.remove('tiktok-mode');
+            body.classList.add('yt-mode');
+            $("#logo-img").attr('src', clipytLogoUrl);
+            $("#get-current-end-btn").show();
+            $("#get-current-start-btn").show();
+        }
+    }
+});
