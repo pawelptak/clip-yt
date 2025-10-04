5d2d1eaa5320a8dd9ff584dd14bf49cb7d9b5230
pawelptak <97pawelptak@gmail.com>
2025-10-04 19:10:36 +0200

    reset quality radio input when disabling

--- src/ClipYT/wwwroot/js/helpers.js
+++ src/ClipYT/wwwroot/js/helpers.js
@@ -1,4 +1,9 @@
 function toggleQualitySelectorAvailability(isEnabled) {
     document.querySelectorAll("#quality-select-container .radio-option")
         .forEach(el => el.classList.toggle("disabled", !isEnabled));
+
+    if (!isEnabled) {
+        const firstQualityRadioInput = document.querySelector("#quality-select-container input[type='radio']");
+        firstQualityRadioInput.checked = true;
+    }
 }
