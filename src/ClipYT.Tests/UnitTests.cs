9b01f4a97f23a2a763b523e1589dd9247167a96d
Paweł Ptak <pawel.ptak@intive.com>
2026-06-12 12:35:34 +0200

    remove tests

--- src/ClipYT.Tests/UnitTests.cs
+++ src/ClipYT.Tests/UnitTests.cs
@@ -237,63 +237,11 @@ namespace ClipYT.Tests
             Assert.True(fileModel.Data.Length > 0);
         }
 
-        [Theory]
-        [InlineData("https://www.youtube.com/watch?v=dQw4w9WgXcQ")]
-        [InlineData("https://www.tiktok.com/@rickastleyofficial/video/7081656622094929158")]
-        [InlineData("https://x.com/i/status/1842206140693664182")]
-        [InlineData("https://www.instagram.com/p/DAEQq8lvpvD/")]
-        [InlineData("https://www.facebook.com/reel/713709415093896")]
-        public async Task ThumbnailService_Should_Return_Valid_Thumbnail_Url(string inputUrl)
-        {
-            // Arrange
-            var httpClientFactory = new HttpClientFactory();
-            var thumbnailService = new ThumbnailService(httpClientFactory);
-            var url = new Uri(inputUrl);
-
-            // Act
-            var thumbnailUrl = await thumbnailService.GetThumbnailUrlAsync(url);
-
-            using var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(thumbnailUrl);
-            var contentType = response.Content.Headers.ContentType?.MediaType;
-            var imageData = await response.Content.ReadAsByteArrayAsync();
-
-            // Assert
-            Assert.NotNull(thumbnailUrl);
-            Assert.True(Uri.TryCreate(thumbnailUrl, UriKind.Absolute, out var resultUri), $"Thumbnail URL is not a valid URI: {thumbnailUrl}");
-            Assert.True(resultUri.Scheme == Uri.UriSchemeHttp || resultUri.Scheme == Uri.UriSchemeHttps, "Thumbnail URL must use HTTP or HTTPS scheme");
-            Assert.True(response.IsSuccessStatusCode, $"Failed to download thumbnail from {thumbnailUrl}. Status code: {response.StatusCode}");
-            Assert.NotNull(contentType);
-            Assert.True(contentType.StartsWith("image/"), $"Content-Type is not an image type: {contentType}");
-            Assert.True(imageData.Length > 0, "Downloaded image data is empty");
-        }
-
-        [Theory]
-        [InlineData("https://www.tiktok.com/@rickastleyofficial/video/7081656622094929158")]
-        [InlineData("https://x.com/i/status/1842206140693664182")]
-        [InlineData("https://www.instagram.com/p/DAEQq8lvpvD/")]
-        [InlineData("https://www.facebook.com/reel/713709415093896")]
-        public async Task ThumbnailService_Should_Return_Thumbnail_For_Social_Media_Urls(string inputUrl)
-        {
-            // Arrange
-            var httpClientFactory = new HttpClientFactory();
-            var thumbnailService = new ThumbnailService(httpClientFactory);
-            var url = new Uri(inputUrl);
-
-            // Act
-            var thumbnailUrl = await thumbnailService.GetThumbnailUrlAsync(url);
-
-            // Assert
-            Assert.NotNull(thumbnailUrl);
-            Assert.NotEmpty(thumbnailUrl);
-            Assert.True(Uri.TryCreate(thumbnailUrl, UriKind.Absolute, out _));
-        }
-
         private static HomeController CreateHomeController(IMediaFileProcessingService mediaFileProcessingService, IHttpClientFactory httpClientFactory)
         {
             var loggerMock = new Mock<ILogger<HomeController>>();
             var memoryCache = new MemoryCache(new MemoryCacheOptions());
-            var thumbnailServiceMock = new Mock<IThumbnailService>();
+            var thumbnailServiceMock = new Mock<IMetadataService>();
             var controller = new HomeController(loggerMock.Object, httpClientFactory, memoryCache, mediaFileProcessingService, thumbnailServiceMock.Object);
             controller.ControllerContext = new ControllerContext
             {
